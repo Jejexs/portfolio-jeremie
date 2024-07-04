4802cf0da5bb9f9452ad9b16cf2d54cd32bda525
Jérémie <jeremie.fournier@hotmail.com>
2024-07-04 23:57:55 +0200

    ajout SpeedInsight Vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { GeistMono } from "geist/font/mono";
 import type { Metadata } from "next";
 import { Anek_Telugu } from "next/font/google";
 import "./globals.css";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 
 const AnekTelugu = Anek_Telugu({ subsets: ["latin"],
@@ -22,7 +23,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="h-full" >
-      <body className={cn(GeistSans.variable, GeistMono.variable, AnekTelugu.variable, "font-sans h-full bg-background text-foreground")}>{children}</body>
+      <body className={cn(GeistSans.variable, GeistMono.variable, AnekTelugu.variable, "font-sans h-full bg-background text-foreground")}>{children}
+      <SpeedInsights />
+      </body>
     </html>
   );
 }
